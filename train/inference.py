28be58b9d922a5ae562bced50e6eca438f66612c
Ben Kreis <ec2-user@ip-172-31-30-210.us-west-2.compute.internal>
2017-12-22 18:45:27 +0000

    Start coding up CNN algo

--- train/inference.py
+++ train/inference.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import h5py
+import math
 import numpy as np
 from numpy.random import seed
 from optparse import OptionParser
@@ -46,6 +47,10 @@ def print_hdf5_item_structure(g, offset='    ') :
             print_hdf5_item_structure(subg, offset + '    ')
             
 
+################
+## Look at h5
+################
+
 #path = "/home/ec2-user/ben_higgstagging/train/train_deep_simple_all/"
 path = "train_simple/"
 h5File = h5py.File(path+'/KERAS_check_model_last_weights.h5')
@@ -56,15 +61,94 @@ h5File = h5py.File(path+'/KERAS_check_model_last_weights.h5')
 
 print_hdf5_file_structure(path+'/KERAS_check_model_last_weights.h5')
     
-#############
-## Extract 
-#############
 
-#Get kernels (aka weights) and biases
+##################################
+## Get weights, biases, and data
+##################################
+
+#Get kernels/filters/weights and biases
 conv_k = h5File['/conv1d_1/conv1d_1/kernel:0'][()]
 conv_b = h5File['/conv1d_1/conv1d_1/bias:0'][()]
 dense_k = h5File['/dense_1/dense_1/kernel:0'][()]
 dense_b = h5File['/dense_1/dense_1/bias:0'][()]
 
 x_train, y_train = get_data()
+
+
+##################
+## Do inference 
+##################
+
+# This is probably written with more lines of code than necessary for future porting to HLS4ML
+
+
+## Settings
+padding = 'same' #same or valid
+stride = 1
+filter_width = 10
+n_filters = 32
+n_channels = 9
+
+
+### Get a sample
+x_sample = x_train[0,:,:]
+y_sample = y_train[0,:]
+
+print "x sample shape: ",x_sample.shape
+print "y sample shape: ",y_sample.shape
+print "x sample: ",x_sample
+print "y sample: ",y_sample
+
+
+### Padding
+### Equations from tensorflow documentation
+
+# Valid
+#out_width  = ceil(float(in_width - filter_width + 1) / float(stride))
+#pad_left = 0
+#pad_right = 0
+
+# Same
+in_width = x_sample.shape[0]
+out_width  = int(math.ceil(float(in_width) / float(stride)))
+if (in_width % stride == 0):
+    pad_along_width = max(filter_width - stride, 0)
+else:
+    pad_along_width = max(filter_width - (in_width % stride), 0)
+pad_left = pad_along_width // 2
+pad_right = pad_along_width - pad_left
+print "in_width: ",in_width
+print "out_width: ",out_width
+print "pad_along_width: ",pad_along_width
+print "pad_left: ",pad_left
+print "pad_right: ", pad_right
+x_sample = np.pad(x_sample, [(pad_left,pad_right),(0,0)], 'constant')
+
+print "x sample shape: ",x_sample.shape
+print "y sample shape: ",y_sample.shape
+print "x sample: ",x_sample
+print "y sample: ",y_sample
+#for j in range(0,x_sample.shape[1]):
+#    for i in range(0,x_sample.shape[0]):
+#        print x_sample[i,j]
+#    print "\n"
+in_width = x_sample.shape[0]
+
+
+##############
+## Convolve 
+##############
+
+for i in range(0,out_width):
+    for f in range(0,n_filters): 
+        for c in range(0,n_channels):
+            
+            #Select data
+            x_buffer = x_sample[:,c]
+            x_buffer = x_buffer[i*stride:i*stride+filter_width]
+
+            #Select filter
+
+        #Sum over channels
+        #Enter output for output position i and filter f
 
